68ccca6b24fd87d9ccbbaf8ea9f8b8e58f03ede1
Romain Catajar <rcatajar@gmail.com>
2015-12-06 15:07:35 +0100

    (wip) indexer - created the class - added the markers

---
+++ indexer.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+
+class Indexer():
+    '''
+    Indexe une collection de documents
+    '''
+
+    # Les marqueurs qui nous intéressent
+    MARKER_NEW_DOC = '.I'
+    MARKER_TITLE = '.T'
+    MARKER_SUMMARY = '.W'
+    MARKER_KEYWORDS = '.K'
+
+    # Les marqueurs à ignorer (cf sujet du projet)
+    MARKER_PUB_DATE = '.B'
+    MARKER_AUTHOR = '.A'
+    MARKER_COLLECTION_DATE = '.N'
+    MARKER_REF = '.X'
+
