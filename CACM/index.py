d13ad55aada4b3d031af7dcd88deeb51a4bc7cc6
Romain Catajar <rcatajar@gmail.com>
2015-12-07 13:58:49 +0100

    Index: better init and add possibility to use external index for tf idf

--- CACM/index.py
+++ CACM/index.py
@@ -16,37 +16,13 @@ class Index(object):
             -> ajoute une liste de documents a l'index
         - add_document(self, document)
             ->ajoute un document a l'index
-        - get_document_vector(self, document_id, weight_type)
+        - get_document_vector(self, document_id, weight_type, index)
             -> retourne vecteur de poids ({mot1: poids1, mot2: poids2, ...}) pour le document id demandé.
-               weight_type indique le type de poids a utiliser
-               ("tf_idf", "tf_idf_normalized", "tf_idf_log", "tf_idf_log_normalized")
+               `weight_type` indique le type de poids a utiliser
+                    ("tf_idf", "tf_idf_normalized", "tf_idf_log", "tf_idf_log_normalized")
+                `index` indique l'index a utiliser pour caculer la dft des mots
+                    (Utile pour indexer une query par rapport a une collection)
     '''
-
-    # L'utilisation de defaultdict permet de "forcer" la structure des dictionnaires d'index
-    # Ce qui permettra d'eviter de devoir a chaque fois checker si les clés / valeurs existent
-    # Comme ca on aura toujours index[doc][mot] = 0 si la valeur n'a pas été set au lieu d'une KeyError
-
-    # Index document -> mots
-    # dictionnaire de la forme:
-    # {
-    #     'id doc': {
-    #         'mot 1': occurence (int)
-    #         'mot 2': occurence (int)
-    #     },
-    #     'id doc 2: {...}
-    # }
-    document_index = defaultdict(lambda: defaultdict(int))
-
-    # Index mots -> document
-    # dictionnaire de la forme:
-    # {
-    #     'mot 1': {
-    #         'id doc': occurence (int)
-    #         'id doc': occurence (int)
-    #     },
-    #     'id doc 2: {...}
-    # }
-    word_index = defaultdict(lambda: defaultdict(int))
 
     # les "stop words" (mots communs a ne pas considérer dans les indexs)
     STOP_WORDS_PATH = './dataset/common_words'
@@ -54,7 +30,38 @@ class Index(object):
 
     def __init__(self, documents=[]):
         self._build_stop_words()
+        self._initialize_indexs()
         self.add_documents(documents)
+
+    def _initialize_indexs(self):
+        '''
+        Initialize les indexs doc -> mots et mots -> docs avec des valeurs par default
+
+        L'utilisation de defaultdict permet de "forcer" la structure des dictionnaires d'index
+        Ce qui permettra d'eviter de devoir a chaque fois checker si les clés / valeurs existent
+        Comme ca on aura toujours index[doc][mot] = 0 si la valeur n'a pas été set au lieu d'une KeyError
+        '''
+        # Index document -> mots
+        # dictionnaire de la forme:
+        # {
+        #     'id doc': {
+        #         'mot 1': occurence (int)
+        #         'mot 2': occurence (int)
+        #     },
+        #     'id doc 2: {...}
+        # }
+        self.document_index = defaultdict(lambda: defaultdict(int))
+
+        # Index mots -> document
+        # dictionnaire de la forme:
+        # {
+        #     'mot 1': {
+        #         'id doc': occurence (int)
+        #         'id doc': occurence (int)
+        #     },
+        #     'id doc 2: {...}
+        # }
+        self.word_index = defaultdict(lambda: defaultdict(int))
 
     @property
     def documents_count(self):
@@ -129,15 +136,16 @@ class Index(object):
         '''
         return len(self.word_index[word].keys())
 
-    def _tf_idf(self, doc_id, normalize):
+    def _tf_idf(self, doc_id, normalize, index):
         '''
         Retourne vecteur avec poids tf-idf pour le document id passé en arguments.
         Poids normalisés si arg `normalize` == True
         '''
         tf_idf = defaultdict(float)
-        documents_count = self.documents_count
+        documents_count = index.documents_count
         for word, count in self.document_index[doc_id].items():
-            dft = self._dft(word)
+            # Float pour forcer une division "non entiere" dans le log de l'idf
+            dft = index._dft(word)
             tf_idf[word] = count * log10(documents_count / dft)
 
         # Normalisation si demandé
@@ -149,15 +157,16 @@ class Index(object):
 
         return tf_idf
 
-    def _tf_idf_log(self, doc_id, normalize):
+    def _tf_idf_log(self, doc_id, normalize, index):
         '''
         Retourne vecteur avec poids tf-idf logarithmique pour le document id passé en arguments.
         Poids normalisés si arg `normalize` == True
         '''
         tf_idf_log = defaultdict(float)
-        documents_count = self.documents_count
+        documents_count = index.documents_count
         for word, count in self.document_index[doc_id].items():
-            dft = float(self._dft(word))
+            # Float pour forcer une division "non entiere" dans le log de l'idf
+            dft = float(index._dft(word))
             tf_idf_log[word] = (1 + log10(count)) * log10(documents_count / dft)
 
         # Normalisation si demandé
@@ -169,21 +178,26 @@ class Index(object):
 
         return tf_idf_log
 
-    def get_document_vector(self, doc_id, weight_type):
+    def get_document_vector(self, doc_id, weight_type, index=None):
         '''
         Retourne vecteur de poids pour le document id demandé.
         (vecteur de la forme {mot1: poids1, mot2: poids2, ...})
 
-        `weight_type` indique le type de poids a utiliser
-        Poids supportés: ["tf_idf", "tf_idf_normalized", "tf_idf_log", "tf_idf_log_normalized"]
+        Args:
+            - `weight_type` indique le type de poids a utiliser
+                Poids supportés: ["tf_idf", "tf_idf_normalized", "tf_idf_log", "tf_idf_log_normalized"]
+            - `index`: index a utliser pour calculer la dft (par default self).
+                Utile pour indexer une query par rapport a l'index d'une collection
         '''
+        index = index or self
+
         if weight_type == 'tf_idf':
-            return self._tf_idf(doc_id, False)
+            return self._tf_idf(doc_id, False, index)
         if weight_type == 'tf_idf_normalized':
-            return self._tf_idf(doc_id, True)
+            return self._tf_idf(doc_id, True, index)
         if weight_type == 'tf_idf_log':
-            return self._tf_idf_log(doc_id, False)
+            return self._tf_idf_log(doc_id, False, index)
         if weight_type == 'tf_idf_log_normalized':
-            return self._tf_idf_log(doc_id, True)
+            return self._tf_idf_log(doc_id, True, index)
         else:
             raise ValueError("Unsupported weight_type: %s" % weight_type)
